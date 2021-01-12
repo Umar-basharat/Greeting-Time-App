e58a21e2d7f59093b350b37992f99ab3f60b705a
Umar-basharat <66340677+Umar-basharat@users.noreply.github.com>
2021-01-12 17:13:41 +0500

    chnge in index

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import './App.css';
 let ctime=new Date();
 ctime=ctime.getHours();
 const cssstyle ={ };
-if (ctime>=6 && ctime<=12) {
+if (ctime>6 && ctime<12) {
   ctime="Good Morning";
   cssstyle.color="green";
 
 }
-else if (ctime>=13 &&  ctime<=16)
+else if (ctime>12 &&  ctime<15)
 {
   ctime="Good After Noon";
   cssstyle.color="yellow";
 }
-else if (ctime>=17 && ctime<=19){
+else if (ctime>15 && ctime<18){
   ctime="Good Evening";
   cssstyle.color="orange";
 
@@ -28,10 +28,12 @@ else{
 ReactDOM.render(
   
 <>
+
 <div className='main'>
-  <h1>Hassn Adeel Sipra </h1>
+
 <h2>Hello Sir,<span style={cssstyle}>   {ctime} </span> </h2> 
 </div>
+
 </>,
 
 document.getElementById('root')
